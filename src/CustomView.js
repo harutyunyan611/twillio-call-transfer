491faa1103301ba3685a12641109c29959a09d3c
Tsovak Harutyunyan <tsovak.harutyunyan@ifyoucan.com>
2019-04-26 00:32:22 +0400

    feat: make dial number dynamic

--- src/CustomView.js
+++ src/CustomView.js
@@ -1,17 +1,32 @@
 import React from 'react';
 
 export default class CustomView extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            num: ''
+        };
+        this.updateInputValue = this.updateInputValue.bind(this);
+        this.transferCall = this.transferCall.bind(this);
+    }
     transferCall() {
-        fetch('https://aureolin-zorse-8830.twil.io/transfer-call')
+        fetch('https://aureolin-zorse-8830.twil.io/transfer-call?number='+this.state.num)
             .then(res => res.json())
             .then(data => data.results);
+    }
+    updateInputValue(evt) {
+        this.setState({
+                          num: evt.target.value
+                      });
     }
     render() {
         return(
             <div>
                 <p>Click the button bellow to transfer the call.</p>
+                <input type="text" value={this.state.num} onChange={this.updateInputValue} />
                 <button onClick={this.transferCall}>Transfer</button>
             </div>
         )
     }
+
 }
